98eea7aa0118086c414688eb6a7da36e4d1269a4
Shiva-kranti <shiva.gupta@krantitechservices.in>
2017-10-04 10:27:26 +0530

    background job for calculate total score

--- src/main/java/com/boilerplate/java/entities/ScoreEntity.java
+++ src/main/java/com/boilerplate/java/entities/ScoreEntity.java
@@ -4,17 +4,37 @@ import java.io.Serializable;
 
 import com.boilerplate.exceptions.rest.ValidationFailedException;
 
+/**
+ * This class define the total score detail
+ * 
+ * @author shiva
+ *
+ */
 public class ScoreEntity extends BaseEntity implements Serializable {
-	
+
+	/**
+	 * This is the user id
+	 */
 	private String userId;
-	
+
+	/**
+	 * This is the max score
+	 */
 	private String maxScore;
-	
+
+	/**
+	 * This is the user total obtained score
+	 */
 	private String obtainedScore;
-	
+
+	/**
+	 * This is the assessment id
+	 */
 	private String assessmentId;
 
 	/**
+	 * This method is used to get the user id
+	 * 
 	 * @return the userId
 	 */
 	public String getUserId() {
@@ -22,13 +42,18 @@ public class ScoreEntity extends BaseEntity implements Serializable {
 	}
 
 	/**
-	 * @param userId the userId to set
+	 * This method is used to set the user id
+	 * 
+	 * @param userId
+	 *            the userId to set
 	 */
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
 
 	/**
+	 * This method is used to get the max score
+	 * 
 	 * @return the maxScore
 	 */
 	public String getMaxScore() {
@@ -36,13 +61,18 @@ public class ScoreEntity extends BaseEntity implements Serializable {
 	}
 
 	/**
-	 * @param maxScore the maxScore to set
+	 * This method is used to set the max score
+	 * 
+	 * @param maxScore
+	 *            the maxScore to set
 	 */
 	public void setMaxScore(String maxScore) {
 		this.maxScore = maxScore;
 	}
 
 	/**
+	 * This method is used to get the user obtained score
+	 * 
 	 * @return the obtainedScore
 	 */
 	public String getObtainedScore() {
@@ -50,13 +80,18 @@ public class ScoreEntity extends BaseEntity implements Serializable {
 	}
 
 	/**
-	 * @param obtainedScore the obtainedScore to set
+	 * This method is used to set the user obtained score
+	 * 
+	 * @param obtainedScore
+	 *            the obtainedScore to set
 	 */
 	public void setObtainedScore(String obtainedScore) {
 		this.obtainedScore = obtainedScore;
 	}
 
 	/**
+	 * This method is used to get the assessment id
+	 * 
 	 * @return the assessmentId
 	 */
 	public String getAssessmentId() {
@@ -64,27 +99,37 @@ public class ScoreEntity extends BaseEntity implements Serializable {
 	}
 
 	/**
-	 * @param assessmentId the assessmentId to set
+	 * This method is used to set the assessment id
+	 * 
+	 * @param assessmentId
+	 *            the assessmentId to set
 	 */
 	public void setAssessmentId(String assessmentId) {
 		this.assessmentId = assessmentId;
 	}
 
+	/**
+	 * @see BaseEntity.validate
+	 */
 	@Override
 	public boolean validate() throws ValidationFailedException {
 		return true;
 	}
 
+	/**
+	 * @see BaseEntity.transformToInternal
+	 */
 	@Override
 	public BaseEntity transformToInternal() {
 		return null;
 	}
 
+	/**
+	 * @see BaseEntity.transformToExternal
+	 */
 	@Override
 	public BaseEntity transformToExternal() {
 		return null;
 	}
-	
-	
 
 }
--- src/main/java/com/boilerplate/service/interfaces/IAssessmentService.java
+++ src/main/java/com/boilerplate/service/interfaces/IAssessmentService.java
@@ -30,9 +30,12 @@ public interface IAssessmentService {
 	 * @throws BadRequestException
 	 *             throw this exception in case of any error while trying to get
 	 *             the assessment data
+	 * @throws ValidationFailedException
+	 *             throw this exception in case of any error while trying to get
+	 *             any assessment which is already submitted by user
 	 */
 	public AssessmentEntity getAssessment(AssessmentEntity assessmentEntity)
-			throws BadRequestException, NotFoundException;
+			throws BadRequestException, NotFoundException, ValidationFailedException;
 
 	/**
 	 * This method is used to get the all assessment which is exist in our
@@ -62,9 +65,9 @@ public interface IAssessmentService {
 	 *            this parameter contains the assessment data which we need to
 	 *            save to data store ,assessment data like assessment id,
 	 *            assessment section,assessment questions etc.
-	 * @throws Exception
-	 *             throw this exception in case of any error while trying to
-	 *             save the assessment data to data store
+	 * @throws ValidationFailedException
+	 *             throw this exception in case of any error in the input data
+	 *             or in assessment entity like if attemptId is null
 	 */
 	public void saveAssesment(AssessmentEntity assessmentEntity) throws ValidationFailedException;
 
@@ -77,9 +80,13 @@ public interface IAssessmentService {
 	 *            this parameter contains the assessment data which we need to
 	 *            save to data store and has information like assessment id,
 	 *            assessment section,assessment questions etc.
+	 * 
+	 * @throws ValidationFailedException
+	 *             throw this exception in case of any error in the input data
+	 *             or in assessment entity like if attemptId is null
 	 * @throws Exception
 	 *             throw this exception in case of any error while trying to
 	 *             save the assessment data to data store
 	 */
-	public void submitAssesment(AssessmentEntity assessmentEntity) throws ValidationFailedException,Exception;
+	public void submitAssesment(AssessmentEntity assessmentEntity) throws ValidationFailedException, Exception;
 }
