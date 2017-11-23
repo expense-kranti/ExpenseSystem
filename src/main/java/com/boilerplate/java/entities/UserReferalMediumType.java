bad58288e070ae26b78cf4ae38b46ef15b5106e4
urvij pratap singh <urvij.singh@krantitechservices.in>
2017-11-23 13:23:30 +0530

    ReferralEntity created

---
+++ src/main/java/com/boilerplate/java/entities/UserReferalMediumType.java
@@ -0,0 +1,21 @@
+package com.boilerplate.java.entities;
+
+/**
+ * This ENUM defines the type of referral medium
+ * @author kranti123
+ *
+ */
+public enum UserReferalMediumType {
+      /**
+       * This is the email as the user referral medium
+       */
+	  Email,
+      /**
+       * This is the phone number as the user referral medium
+       */
+	  Phone_Number,
+	  /**
+	   * This is the facebook as the user referral medium
+	   */
+	  Facebook
+}
