03c6fc52dff30a6fe71ea30ba8e3c3b592fab501
urvij pratap singh <urvij.singh@krantitechservices.in>
2018-01-15 13:15:42 +0530

    added empty tax entity

---
+++ src/main/java/com/boilerplate/java/entities/TaxEntity.java
@@ -0,0 +1,25 @@
+package com.boilerplate.java.entities;
+
+import com.boilerplate.exceptions.rest.ValidationFailedException;
+
+public class TaxEntity extends BaseEntity{
+
+	@Override
+	public boolean validate() throws ValidationFailedException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public BaseEntity transformToInternal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BaseEntity transformToExternal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
